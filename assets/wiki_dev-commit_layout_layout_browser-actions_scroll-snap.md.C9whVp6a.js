47c436dcda663471453a2493a63ad7bc40d65226
Anton Sergeenkov <asergeenkov@combotech.net>
2026-03-08 23:36:17 +0400

    Updates

---
+++ assets/wiki_dev-commit_layout_layout_browser-actions_scroll-snap.md.C9whVp6a.js
@@ -0,0 +1,11 @@
+import{_ as a,o as n,c as l,aw as o}from"./chunks/framework.DLufY-ao.js";const _=JSON.parse('{"title":"scroll-snap","description":"","frontmatter":{},"headers":[],"relativePath":"wiki/dev-commit/layout/layout/browser-actions/scroll-snap.md","filePath":"wiki/dev-commit/layout/layout/browser-actions/scroll-snap.md"}'),p={name:"wiki/dev-commit/layout/layout/browser-actions/scroll-snap.md"};function e(c,s,t,r,i,d){return n(),l("div",null,[...s[0]||(s[0]=[o(`<h1 id="scroll-snap" tabindex="-1">scroll-snap <a class="header-anchor" href="#scroll-snap" aria-label="Permalink to &quot;scroll-snap&quot;">​</a></h1><ul><li>Контроль поведения скролла</li></ul><div class="language-css"><button title="Copy Code" class="copy"></button><span class="lang">css</span><pre class="shiki one-light vp-code" tabindex="0"><code><span class="line"><span style="color:#E45649;">div</span><span style="color:#383A42;"> {</span></span>
+<span class="line"><span style="color:#383A42;">  scroll-snap-type: ...;</span></span>
+<span class="line"><span style="color:#383A42;">  scroll-padding: ...;</span></span>
+<span class="line"><span style="color:#383A42;">  scroll-snap-align: ...;</span></span>
+<span class="line"><span style="color:#383A42;">}</span></span>
+<span class="line"><span style="color:#986801;">.container</span><span style="color:#383A42;"> {</span></span>
+<span class="line"><span style="color:#383A42;">  scroll-snap-type: y mandatory;</span></span>
+<span class="line"><span style="color:#383A42;">}</span></span>
+<span class="line"><span style="color:#986801;">.child</span><span style="color:#383A42;"> {</span></span>
+<span class="line"><span style="color:#383A42;">  scroll-snap-align: start;</span></span>
+<span class="line"><span style="color:#383A42;">}</span></span></code></pre></div>`,3)])])}const u=a(p,[["render",e]]);export{_ as __pageData,u as default};
