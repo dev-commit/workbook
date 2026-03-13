1b7eaa86a6e7b7f76b28a70ff56ea5afe80e4562
Anton Sergeenkov <asergeenkov@combotech.net>
2026-03-13 12:38:18 +0400

    Updates

---
+++ assets/wiki_dev-commit_layout_sass_functions_at-root.md.Z9Mk7dCM.lean.js
@@ -0,0 +1,29 @@
+import{_ as t,C as c,o as r,c as p,aw as i,E as e,w as a,j as s,a as l}from"./chunks/framework.DLufY-ao.js";const f=JSON.parse('{"title":"Директива @at-root","description":"","frontmatter":{},"headers":[],"relativePath":"wiki/dev-commit/layout/sass/functions/at-root.md","filePath":"wiki/dev-commit/layout/sass/functions/at-root.md"}'),y={name:"wiki/dev-commit/layout/sass/functions/at-root.md"};function d(A,o,u,h,g,m){const n=c("v-two");return r(),p("div",null,[o[4]||(o[4]=i("",4)),e(n,{compare:"",title:["Sass","CSS"]},{first:a(()=>[...o[0]||(o[0]=[s("div",{class:"language-scss"},[s("button",{title:"Copy Code",class:"copy"}),s("span",{class:"lang"},"scss"),s("pre",{class:"shiki one-light vp-code",tabindex:"0"},[s("code",null,[s("span",{class:"line"},[s("span",{style:{color:"#986801"}},".parent"),s("span",{style:{color:"#383A42"}}," {")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#383A42"}},"  color: "),s("span",{style:{color:"#986801"}},"red"),s("span",{style:{color:"#383A42"}},";")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#A626A4"}},"  @at-root"),s("span",{style:{color:"#986801"}}," .child"),s("span",{style:{color:"#383A42"}}," {")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#383A42"}},"    color: "),s("span",{style:{color:"#986801"}},"green"),s("span",{style:{color:"#383A42"}},";")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#383A42"}},"  }")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#383A42"}},"}")])])])],-1)])]),last:a(()=>[...o[1]||(o[1]=[s("div",{class:"language-css"},[s("button",{title:"Copy Code",class:"copy"}),s("span",{class:"lang"},"css"),s("pre",{class:"shiki one-light vp-code",tabindex:"0"},[s("code",null,[s("span",{class:"line"},[s("span",{style:{color:"#986801"}},".parent"),s("span",{style:{color:"#383A42"}}," {")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#383A42"}},"  color: "),s("span",{style:{color:"#986801"}},"red"),s("span",{style:{color:"#383A42"}},";")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#383A42"}},"}")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#986801"}},".child"),s("span",{style:{color:"#383A42"}}," {")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#383A42"}},"  color: "),s("span",{style:{color:"#986801"}},"green"),s("span",{style:{color:"#383A42"}},";")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#383A42"}},"}")])])])],-1)])]),_:1}),e(n,{compare:"",title:["Sass","CSS"]},{first:a(()=>[...o[2]||(o[2]=[s("div",{class:"language-scss"},[s("button",{title:"Copy Code",class:"copy"}),s("span",{class:"lang"},"scss"),s("pre",{class:"shiki one-light vp-code",tabindex:"0"},[s("code",null,[s("span",{class:"line"},[s("span",{style:{color:"#986801"}},".parent"),s("span",{style:{color:"#383A42"}}," {")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#383A42"}},"  color: "),s("span",{style:{color:"#986801"}},"red"),s("span",{style:{color:"#383A42"}},";")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#A626A4"}},"  @at-root"),s("span",{style:{color:"#383A42"}}," {")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#986801"}},"    .child1"),s("span",{style:{color:"#383A42"}}," {")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#383A42"}},"      color: "),s("span",{style:{color:"#986801"}},"green"),s("span",{style:{color:"#383A42"}},";")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#383A42"}},"    }")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#986801"}},"    .child2"),s("span",{style:{color:"#383A42"}}," {")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#383A42"}},"      color: "),s("span",{style:{color:"#986801"}},"orange"),s("span",{style:{color:"#383A42"}},";")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#383A42"}},"    }")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#383A42"}},"  }")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#383A42"}},"}")])])])],-1)])]),last:a(()=>[...o[3]||(o[3]=[s("div",{class:"language-css"},[s("button",{title:"Copy Code",class:"copy"}),s("span",{class:"lang"},"css"),s("pre",{class:"shiki one-light vp-code",tabindex:"0"},[s("code",null,[s("span",{class:"line"},[s("span",{style:{color:"#986801"}},".parent"),s("span",{style:{color:"#383A42"}}," {")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#383A42"}},"  color: "),s("span",{style:{color:"#986801"}},"red"),s("span",{style:{color:"#383A42"}},";")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#383A42"}},"}")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#986801"}},".child1"),s("span",{style:{color:"#383A42"}}," {")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#383A42"}},"  color: "),s("span",{style:{color:"#986801"}},"green"),s("span",{style:{color:"#383A42"}},";")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#383A42"}},"}")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#986801"}},".child2"),s("span",{style:{color:"#383A42"}}," {")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#383A42"}},"  color: "),s("span",{style:{color:"#986801"}},"orange"),s("span",{style:{color:"#383A42"}},";")]),l(`
+`),s("span",{class:"line"},[s("span",{style:{color:"#383A42"}},"}")])])])],-1)])]),_:1})])}const v=t(y,[["render",d]]);export{f as __pageData,v as default};
