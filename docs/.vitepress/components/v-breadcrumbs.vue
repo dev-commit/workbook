396f2a28b4da8f44a20530f11ddf53dee61f3272
Anton Sergeenkov <asergeenkov@combotech.net>
2026-03-16 00:48:12 +0400

    Bash Script Commit

--- docs/.vitepress/components/v-breadcrumbs.vue
+++ docs/.vitepress/components/v-breadcrumbs.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrapper">
     <span v-for="(item, i) in items" :key="i">
-      <v-code theme="text">{{ item }}</v-code>
+      <span class="code">{{ item }}</span>
 
       <template v-if="i !== items.length - 1">
         <span v-html="getSymbol()"></span>
@@ -34,23 +34,17 @@ const getSymbol = (): string => {
   color: var(--vp-c-text-2);
 }
 
-:deep(.u-code-text),
-:deep(.u-code-light) {
-  font-family: monospace;
-  padding: 0 5px;
-  border-radius: 3px;
-  line-height: 1.4em;
+.code {
+  font-family: var(--vp-font-family-mono);
+  border-radius: 6px;
   font-size: 14px;
-}
-:deep(.u-code-text) {
-  border: 1px solid;
-  border-color: #dfe1bf;
-  background-color: #f0f1e1;
-  color: rgb(var(--color-orange));
-}
-:deep(.u-code-light) {
-  background-color: rgb(var(--color-white));
-  color: rgb(var(--color-orange));
-  box-shadow: 0.1px 0.1px 5px 1px rgba(var(--color-black), 0.15);
+  border-bottom: 2px solid rgb(229, 231, 235);
+  color: rgb(82, 82, 91);
+  box-shadow:
+    0px 0px 5px 0px #00000005,
+    0px 2px 10px 0px #0000000f,
+    0px 0px 1px 0px #0000004d;
+  background-color: rgb(244, 244, 245);
+  padding: 3px 6px;
 }
 </style>
