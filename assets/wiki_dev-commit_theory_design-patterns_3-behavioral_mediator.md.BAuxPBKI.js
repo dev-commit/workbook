d461abc22e8dc35c0e10ca91918ddf7cd2601c01
Anton Sergeenkov <asergeenkov@combotech.net>
2026-04-05 13:54:34 +0400

    Updates

---
+++ assets/wiki_dev-commit_theory_design-patterns_3-behavioral_mediator.md.BAuxPBKI.js
@@ -0,0 +1,59 @@
+import{_ as n,o as a,c as l,aw as o}from"./chunks/framework.DLufY-ao.js";const d=JSON.parse('{"title":"Mediator (Посредник)","description":"","frontmatter":{},"headers":[],"relativePath":"wiki/dev-commit/theory/design-patterns/3-behavioral/mediator.md","filePath":"wiki/dev-commit/theory/design-patterns/3-behavioral/mediator.md"}'),p={name:"wiki/dev-commit/theory/design-patterns/3-behavioral/mediator.md"};function e(t,s,c,r,y,i){return a(),l("div",null,[...s[0]||(s[0]=[o(`<h1 id="mediator-посредник" tabindex="-1">Mediator (Посредник) <a class="header-anchor" href="#mediator-посредник" aria-label="Permalink to &quot;Mediator (Посредник)&quot;">​</a></h1><h2 id="информация" tabindex="-1">Информация <a class="header-anchor" href="#информация" aria-label="Permalink to &quot;Информация&quot;">​</a></h2><div class="tip custom-block"><p class="custom-block-title">TIP</p></div><ul><li>Mediator - поволяет устанавливать плотную и тесную коммуникация между объектами разного типа</li><li>При этом, паттерн предоставляет центрелизованную абстракцию, которая позволяет взаимодействовать группе объектов через друг друга</li></ul><h2 id="примеры" tabindex="-1">Примеры <a class="header-anchor" href="#примеры" aria-label="Permalink to &quot;Примеры&quot;">​</a></h2><h3 id="чат" tabindex="-1">Чат <a class="header-anchor" href="#чат" aria-label="Permalink to &quot;Чат&quot;">​</a></h3><ul><li>Приложение позволяет создавать новых пользователей и присоединять их к чату</li><li>Каждый сможет отправлять сообщения друг другу</li></ul><div class="language-js"><button title="Copy Code" class="copy"></button><span class="lang">js</span><pre class="shiki one-light vp-code" tabindex="0"><code><span class="line"><span style="color:#A626A4;">class</span><span style="color:#C18401;"> User</span><span style="color:#383A42;"> {</span></span>
+<span class="line"><span style="color:#A626A4;">  constructor</span><span style="color:#383A42;">(name) {</span></span>
+<span class="line"><span style="color:#E45649;">    this</span><span style="color:#383A42;">.</span><span style="color:#E45649;">name</span><span style="color:#0184BC;"> =</span><span style="color:#383A42;"> name;</span></span>
+<span class="line"><span style="color:#E45649;">    this</span><span style="color:#383A42;">.</span><span style="color:#E45649;">room</span><span style="color:#0184BC;"> =</span><span style="color:#986801;"> null</span><span style="color:#383A42;">;</span></span>
+<span class="line"><span style="color:#383A42;">  }</span></span>
+<span class="line"><span style="color:#A0A1A7;font-style:italic;">  // Отправка сообщения (message - сообщение, to - кому отправить)</span></span>
+<span class="line"><span style="color:#4078F2;">  send</span><span style="color:#383A42;">(message, to) {</span></span>
+<span class="line"><span style="color:#A0A1A7;font-style:italic;">    // (message - сообщение, this - от кого, to - кому)</span></span>
+<span class="line"><span style="color:#E45649;">    this</span><span style="color:#383A42;">.</span><span style="color:#E45649;">room</span><span style="color:#383A42;">.</span><span style="color:#4078F2;">send</span><span style="color:#383A42;">(message, </span><span style="color:#E45649;">this</span><span style="color:#383A42;">, to);</span></span>
+<span class="line"><span style="color:#383A42;">  }</span></span>
+<span class="line"><span style="color:#A0A1A7;font-style:italic;">  // Получение сообщения (message - сообщение, from - от кого)</span></span>
+<span class="line"><span style="color:#4078F2;">  receive</span><span style="color:#383A42;">(message, from) {</span></span>
+<span class="line"><span style="color:#383A42;">    console.</span><span style="color:#4078F2;">log</span><span style="color:#383A42;">(</span><span style="color:#50A14F;">\`</span><span style="color:#CA1243;">\${</span><span style="color:#383A42;">from</span><span style="color:#50A14F;">.</span><span style="color:#E45649;">name</span><span style="color:#CA1243;">}</span><span style="color:#50A14F;"> =&gt; </span><span style="color:#CA1243;">\${</span><span style="color:#E45649;">this</span><span style="color:#50A14F;">.</span><span style="color:#E45649;">name</span><span style="color:#CA1243;">}</span><span style="color:#50A14F;">: </span><span style="color:#CA1243;">\${</span><span style="color:#383A42;">message</span><span style="color:#CA1243;">}</span><span style="color:#50A14F;">\`</span><span style="color:#383A42;">);</span></span>
+<span class="line"><span style="color:#383A42;">  }</span></span>
+<span class="line"><span style="color:#383A42;">}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A626A4;">class</span><span style="color:#C18401;"> ChatRoom</span><span style="color:#383A42;"> {</span></span>
+<span class="line"><span style="color:#A626A4;">  constructor</span><span style="color:#383A42;">() {</span></span>
+<span class="line"><span style="color:#E45649;">    this</span><span style="color:#383A42;">.</span><span style="color:#E45649;">users</span><span style="color:#0184BC;"> =</span><span style="color:#383A42;"> {};</span></span>
+<span class="line"><span style="color:#383A42;">  }</span></span>
+<span class="line"><span style="color:#A0A1A7;font-style:italic;">  // Регистрация новых пользователей</span></span>
+<span class="line"><span style="color:#4078F2;">  register</span><span style="color:#383A42;">(user) {</span></span>
+<span class="line"><span style="color:#E45649;">    this</span><span style="color:#383A42;">.</span><span style="color:#E45649;">users</span><span style="color:#383A42;">[user.</span><span style="color:#C18401;">name</span><span style="color:#383A42;">] </span><span style="color:#0184BC;">=</span><span style="color:#383A42;"> user;</span></span>
+<span class="line"><span style="color:#383A42;">    user.</span><span style="color:#E45649;">room</span><span style="color:#0184BC;"> =</span><span style="color:#E45649;"> this</span><span style="color:#383A42;">;</span></span>
+<span class="line"><span style="color:#383A42;">  }</span></span>
+<span class="line"><span style="color:#4078F2;">  send</span><span style="color:#383A42;">(message, from, to) {</span></span>
+<span class="line"><span style="color:#A626A4;">    if</span><span style="color:#383A42;"> (to) {</span></span>
+<span class="line"><span style="color:#383A42;">      to.</span><span style="color:#4078F2;">receive</span><span style="color:#383A42;">(message, from);</span></span>
+<span class="line"><span style="color:#383A42;">    } </span><span style="color:#A626A4;">else</span><span style="color:#383A42;"> {</span></span>
+<span class="line"><span style="color:#A0A1A7;font-style:italic;">      // Отправить всем пользователем в данной комнате кроме текущего пользователя</span></span>
+<span class="line"><span style="color:#383A42;">      Object.</span><span style="color:#4078F2;">keys</span><span style="color:#383A42;">(</span><span style="color:#E45649;">this</span><span style="color:#383A42;">.</span><span style="color:#E45649;">users</span><span style="color:#383A42;">).</span><span style="color:#4078F2;">forEach</span><span style="color:#383A42;">((key) </span><span style="color:#A626A4;">=&gt;</span><span style="color:#383A42;"> {</span></span>
+<span class="line"><span style="color:#A626A4;">        if</span><span style="color:#383A42;"> (</span><span style="color:#E45649;">this</span><span style="color:#383A42;">.</span><span style="color:#E45649;">users</span><span style="color:#383A42;">[key] </span><span style="color:#0184BC;">!==</span><span style="color:#383A42;"> from) {</span></span>
+<span class="line"><span style="color:#E45649;">          this</span><span style="color:#383A42;">.</span><span style="color:#E45649;">users</span><span style="color:#383A42;">[key].</span><span style="color:#4078F2;">receive</span><span style="color:#383A42;">(message, from);</span></span>
+<span class="line"><span style="color:#383A42;">        }</span></span>
+<span class="line"><span style="color:#383A42;">      });</span></span>
+<span class="line"><span style="color:#383A42;">    }</span></span>
+<span class="line"><span style="color:#383A42;">  }</span></span>
+<span class="line"><span style="color:#383A42;">}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A626A4;">const</span><span style="color:#986801;"> vlad</span><span style="color:#0184BC;"> =</span><span style="color:#A626A4;"> new</span><span style="color:#4078F2;"> User</span><span style="color:#383A42;">(</span><span style="color:#50A14F;">&quot;Vladilen&quot;</span><span style="color:#383A42;">);</span></span>
+<span class="line"><span style="color:#A626A4;">const</span><span style="color:#986801;"> lena</span><span style="color:#0184BC;"> =</span><span style="color:#A626A4;"> new</span><span style="color:#4078F2;"> User</span><span style="color:#383A42;">(</span><span style="color:#50A14F;">&quot;Elena&quot;</span><span style="color:#383A42;">);</span></span>
+<span class="line"><span style="color:#A626A4;">const</span><span style="color:#986801;"> igor</span><span style="color:#0184BC;"> =</span><span style="color:#A626A4;"> new</span><span style="color:#4078F2;"> User</span><span style="color:#383A42;">(</span><span style="color:#50A14F;">&quot;Igor&quot;</span><span style="color:#383A42;">);</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A626A4;">const</span><span style="color:#986801;"> room</span><span style="color:#0184BC;"> =</span><span style="color:#A626A4;"> new</span><span style="color:#4078F2;"> ChatRoom</span><span style="color:#383A42;">();</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A0A1A7;font-style:italic;">// Регистрация пользователей в одну комнату</span></span>
+<span class="line"><span style="color:#383A42;">room.</span><span style="color:#4078F2;">register</span><span style="color:#383A42;">(vlad);</span></span>
+<span class="line"><span style="color:#383A42;">room.</span><span style="color:#4078F2;">register</span><span style="color:#383A42;">(lena);</span></span>
+<span class="line"><span style="color:#383A42;">room.</span><span style="color:#4078F2;">register</span><span style="color:#383A42;">(igor);</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#383A42;">vlad.</span><span style="color:#4078F2;">send</span><span style="color:#383A42;">(</span><span style="color:#50A14F;">&quot;Hello!&quot;</span><span style="color:#383A42;">, lena);</span></span>
+<span class="line"><span style="color:#383A42;">lena.</span><span style="color:#4078F2;">send</span><span style="color:#383A42;">(</span><span style="color:#50A14F;">&quot;Hello hello!&quot;</span><span style="color:#383A42;">, vlad);</span></span>
+<span class="line"><span style="color:#383A42;">igor.</span><span style="color:#4078F2;">send</span><span style="color:#383A42;">(</span><span style="color:#50A14F;">&quot;Vsem privet&quot;</span><span style="color:#383A42;">);</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A0A1A7;font-style:italic;">// Output:</span></span>
+<span class="line"><span style="color:#A0A1A7;font-style:italic;">// &quot;Vladilen =&gt; Elena: Hello!&quot;</span></span>
+<span class="line"><span style="color:#A0A1A7;font-style:italic;">// &quot;Elena =&gt; Vladilen: Hello hello!&quot;</span></span>
+<span class="line"><span style="color:#A0A1A7;font-style:italic;">// &quot;Igor =&gt; Vladilen: Vsem privet&quot;</span></span>
+<span class="line"><span style="color:#A0A1A7;font-style:italic;">// &quot;Igor =&gt; Elena: Vsem privet&quot;</span></span></code></pre></div>`,8)])])}const m=n(p,[["render",e]]);export{d as __pageData,m as default};
---
+++ assets/wiki_dev-commit_theory_design-patterns_3-behavioral_strategy.md.BDHJ5Z8J.js
@@ -0,0 +1,42 @@
+import{_ as a,o as n,c as l,aw as p}from"./chunks/framework.DLufY-ao.js";const d=JSON.parse('{"title":"Strategy (Стратегия)","description":"","frontmatter":{},"headers":[],"relativePath":"wiki/dev-commit/theory/design-patterns/3-behavioral/strategy.md","filePath":"wiki/dev-commit/theory/design-patterns/3-behavioral/strategy.md"}'),o={name:"wiki/dev-commit/theory/design-patterns/3-behavioral/strategy.md"};function e(t,s,c,r,y,i){return n(),l("div",null,[...s[0]||(s[0]=[p(`<h1 id="strategy-стратегия" tabindex="-1">Strategy (Стратегия) <a class="header-anchor" href="#strategy-стратегия" aria-label="Permalink to &quot;Strategy (Стратегия)&quot;">​</a></h1><h2 id="информация" tabindex="-1">Информация <a class="header-anchor" href="#информация" aria-label="Permalink to &quot;Информация&quot;">​</a></h2><div class="tip custom-block"><p class="custom-block-title">TIP</p></div><ul><li>Strategy - взаимодействие с разыными стратегиями через один интерфейс</li><li>Позволяет создавать оболочку для различных интерфейсов для возможности использования разных алгоритмов и интерфейсов в задаче</li><li>Определяет семейство алгоритмов, которые наследуют объекты в неизменяемом порядке</li></ul><h2 id="примеры" tabindex="-1">Примеры <a class="header-anchor" href="#примеры" aria-label="Permalink to &quot;Примеры&quot;">​</a></h2><h3 id="стратегия-поездки" tabindex="-1">Стратегия поездки <a class="header-anchor" href="#стратегия-поездки" aria-label="Permalink to &quot;Стратегия поездки&quot;">​</a></h3><div class="language-js"><button title="Copy Code" class="copy"></button><span class="lang">js</span><pre class="shiki one-light vp-code" tabindex="0"><code><span class="line"><span style="color:#A626A4;">class</span><span style="color:#C18401;"> Vehicle</span><span style="color:#383A42;"> {</span></span>
+<span class="line"><span style="color:#4078F2;">  travelTime</span><span style="color:#383A42;">() {</span></span>
+<span class="line"><span style="color:#A626A4;">    return</span><span style="color:#E45649;"> this</span><span style="color:#383A42;">.</span><span style="color:#E45649;">timeTaken</span><span style="color:#383A42;">;</span></span>
+<span class="line"><span style="color:#383A42;">  }</span></span>
+<span class="line"><span style="color:#383A42;">}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A0A1A7;font-style:italic;">// Первая стратегия</span></span>
+<span class="line"><span style="color:#A626A4;">class</span><span style="color:#C18401;"> Bus</span><span style="color:#A626A4;"> extends</span><span style="color:#C18401;"> Vehicle</span><span style="color:#383A42;"> {</span></span>
+<span class="line"><span style="color:#A626A4;">  constructor</span><span style="color:#383A42;">() {</span></span>
+<span class="line"><span style="color:#E45649;">    super</span><span style="color:#383A42;">();</span></span>
+<span class="line"><span style="color:#E45649;">    this</span><span style="color:#383A42;">.</span><span style="color:#E45649;">timeTaken</span><span style="color:#0184BC;"> =</span><span style="color:#986801;"> 10</span><span style="color:#383A42;">;</span></span>
+<span class="line"><span style="color:#383A42;">  }</span></span>
+<span class="line"><span style="color:#383A42;">}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A0A1A7;font-style:italic;">// Вторая стратегия</span></span>
+<span class="line"><span style="color:#A626A4;">class</span><span style="color:#C18401;"> Taxi</span><span style="color:#A626A4;"> extends</span><span style="color:#C18401;"> Vehicle</span><span style="color:#383A42;"> {</span></span>
+<span class="line"><span style="color:#A626A4;">  constructor</span><span style="color:#383A42;">() {</span></span>
+<span class="line"><span style="color:#E45649;">    super</span><span style="color:#383A42;">();</span></span>
+<span class="line"><span style="color:#E45649;">    this</span><span style="color:#383A42;">.</span><span style="color:#E45649;">timeTaken</span><span style="color:#0184BC;"> =</span><span style="color:#986801;"> 5</span><span style="color:#383A42;">;</span></span>
+<span class="line"><span style="color:#383A42;">  }</span></span>
+<span class="line"><span style="color:#383A42;">}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A0A1A7;font-style:italic;">// Третья стратегия</span></span>
+<span class="line"><span style="color:#A626A4;">class</span><span style="color:#C18401;"> Car</span><span style="color:#A626A4;"> extends</span><span style="color:#C18401;"> Vehicle</span><span style="color:#383A42;"> {</span></span>
+<span class="line"><span style="color:#A626A4;">  constructor</span><span style="color:#383A42;">() {</span></span>
+<span class="line"><span style="color:#E45649;">    super</span><span style="color:#383A42;">();</span></span>
+<span class="line"><span style="color:#E45649;">    this</span><span style="color:#383A42;">.</span><span style="color:#E45649;">timeTaken</span><span style="color:#0184BC;"> =</span><span style="color:#986801;"> 3</span><span style="color:#383A42;">;</span></span>
+<span class="line"><span style="color:#383A42;">  }</span></span>
+<span class="line"><span style="color:#383A42;">}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A0A1A7;font-style:italic;">// Время на дорогу</span></span>
+<span class="line"><span style="color:#A626A4;">class</span><span style="color:#C18401;"> Commute</span><span style="color:#383A42;"> {</span></span>
+<span class="line"><span style="color:#4078F2;">  travel</span><span style="color:#383A42;">(transport) {</span></span>
+<span class="line"><span style="color:#A626A4;">    return</span><span style="color:#383A42;"> transport.</span><span style="color:#4078F2;">travelTime</span><span style="color:#383A42;">();</span></span>
+<span class="line"><span style="color:#383A42;">  }</span></span>
+<span class="line"><span style="color:#383A42;">}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A626A4;">const</span><span style="color:#986801;"> commute</span><span style="color:#0184BC;"> =</span><span style="color:#A626A4;"> new</span><span style="color:#4078F2;"> Commute</span><span style="color:#383A42;">();</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#383A42;">console.</span><span style="color:#4078F2;">log</span><span style="color:#383A42;">(commute.</span><span style="color:#4078F2;">travel</span><span style="color:#383A42;">(</span><span style="color:#A626A4;">new</span><span style="color:#4078F2;"> Taxi</span><span style="color:#383A42;">())); </span><span style="color:#A0A1A7;font-style:italic;">// =&gt; 5</span></span>
+<span class="line"><span style="color:#383A42;">console.</span><span style="color:#4078F2;">log</span><span style="color:#383A42;">(commute.</span><span style="color:#4078F2;">travel</span><span style="color:#383A42;">(</span><span style="color:#A626A4;">new</span><span style="color:#4078F2;"> Bus</span><span style="color:#383A42;">())); </span><span style="color:#A0A1A7;font-style:italic;">// =&gt; 10</span></span>
+<span class="line"><span style="color:#383A42;">console.</span><span style="color:#4078F2;">log</span><span style="color:#383A42;">(commute.</span><span style="color:#4078F2;">travel</span><span style="color:#383A42;">(</span><span style="color:#A626A4;">new</span><span style="color:#4078F2;"> Car</span><span style="color:#383A42;">())); </span><span style="color:#A0A1A7;font-style:italic;">// =&gt; 3</span></span></code></pre></div>`,7)])])}const m=a(o,[["render",e]]);export{d as __pageData,m as default};
