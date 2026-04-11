d36127c86acf0e0b3417ae6fd82c81372ec327aa
Anton Sergeenkov <asergeenkov@combotech.net>
2026-04-11 13:32:08 +0400

    Bash Script Commit

--- docs/wiki/dev-commit/network/network/build-network.ts
+++ docs/wiki/dev-commit/network/network/build-network.ts
@@ -15,20 +15,20 @@ const topics: ISidebarTopics[] = [
   {
     title: "HTTP",
     children: [
-      ["http/http", `${emoji.fire}HTTP`],
+      ["http/http", emoji.fire + "HTTP / TCP"],
       ["http/http2", "HTTP/2"],
       ["http/https", "HTTPS"],
-      ["http/http-and-https", "HTTP и HTTPS"],
-      ["http/1-http-structure", `${emoji.star}1. Структура протокола`],
-      ["http/2-http-methods", `${emoji.star}2. HTTP Methods`],
-      ["http/3-http-headers", `${emoji.star}3. HTTP Headers`],
-      ["http/4-http-status-codes", `${emoji.star}4. HTTP Status Codes`],
+      ["http/http-vs-https", "HTTP и HTTPS"],
+      ["http/1-http-structure", emoji.star + "1. Структура протокола"],
+      ["http/2-http-methods", emoji.star + "2. HTTP Methods"],
+      ["http/3-http-headers", emoji.star + "3. HTTP Headers"],
+      ["http/4-http-status-codes", emoji.star + "4. HTTP Status Codes"],
     ],
   },
   {
     title: "Протоколы",
     children: [
-      ["protocols/osi", `${emoji.theory}Сетевая модель OSI`],
+      ["protocols/osi", emoji.theory + "Сетевая модель OSI"],
       ["protocols/tcp", "TCP"],
       ["protocols/udp", "UDP"],
     ],
