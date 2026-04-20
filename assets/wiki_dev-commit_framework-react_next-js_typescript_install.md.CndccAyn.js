e95b1221db31fc8ae238f7d12d82bcd9f1abfa7e
Anton Sergeenkov <asergeenkov@combotech.net>
2026-04-20 18:16:52 +0400

    Updates

---
+++ assets/wiki_dev-commit_framework-react_next-js_typescript_install.md.CndccAyn.js
@@ -0,0 +1,21 @@
+import{_ as n,o as a,c as o,aw as p}from"./chunks/framework.DLufY-ao.js";const q=JSON.parse('{"title":"Установка и настройка","description":"","frontmatter":{},"headers":[],"relativePath":"wiki/dev-commit/framework-react/next-js/typescript/install.md","filePath":"wiki/dev-commit/framework-react/next-js/typescript/install.md"}'),l={name:"wiki/dev-commit/framework-react/next-js/typescript/install.md"};function t(e,s,c,r,i,y){return a(),o("div",null,[...s[0]||(s[0]=[p(`<h1 id="установка-и-настроика" tabindex="-1">Установка и настройка <a class="header-anchor" href="#установка-и-настроика" aria-label="Permalink to &quot;Установка и настройка&quot;">​</a></h1><div class="language-bash"><button title="Copy Code" class="copy"></button><span class="lang">bash</span><pre class="shiki one-light vp-code" tabindex="0"><code><span class="line"><span style="color:#4078F2;">npm</span><span style="color:#50A14F;"> i</span><span style="color:#986801;"> --save-dev</span></span>
+<span class="line"><span style="color:#4078F2;">  typescript</span></span>
+<span class="line"><span style="color:#4078F2;">  @types/node</span></span>
+<span class="line"><span style="color:#4078F2;">  @types/react</span></span>
+<span class="line"><span style="color:#4078F2;">  @types/react-dom</span></span>
+<span class="line"><span style="color:#4078F2;">  package.json</span></span></code></pre></div><div class="language-json"><button title="Copy Code" class="copy"></button><span class="lang">json</span><pre class="shiki one-light vp-code" tabindex="0"><code><span class="line"><span style="color:#383A42;">{</span></span>
+<span class="line"><span style="color:#E45649;">  &quot;dependencies&quot;</span><span style="color:#383A42;">: {</span></span>
+<span class="line"><span style="color:#E45649;">    &quot;react&quot;</span><span style="color:#383A42;">: </span><span style="color:#50A14F;">&quot;^18&quot;</span><span style="color:#383A42;">,</span></span>
+<span class="line"><span style="color:#E45649;">    &quot;react-dom&quot;</span><span style="color:#383A42;">: </span><span style="color:#50A14F;">&quot;^18&quot;</span><span style="color:#383A42;">,</span></span>
+<span class="line"><span style="color:#E45649;">    &quot;next&quot;</span><span style="color:#383A42;">: </span><span style="color:#50A14F;">&quot;14.2.4&quot;</span></span>
+<span class="line"><span style="color:#383A42;">  },</span></span>
+<span class="line"><span style="color:#E45649;">  &quot;devDependencies&quot;</span><span style="color:#383A42;">: {</span></span>
+<span class="line"><span style="color:#E45649;">    &quot;typescript&quot;</span><span style="color:#383A42;">: </span><span style="color:#50A14F;">&quot;^5&quot;</span><span style="color:#383A42;">,</span></span>
+<span class="line"><span style="color:#E45649;">    &quot;@types/node&quot;</span><span style="color:#383A42;">: </span><span style="color:#50A14F;">&quot;^20&quot;</span><span style="color:#383A42;">,</span></span>
+<span class="line"><span style="color:#E45649;">    &quot;@types/react&quot;</span><span style="color:#383A42;">: </span><span style="color:#50A14F;">&quot;^18&quot;</span><span style="color:#383A42;">,</span></span>
+<span class="line"><span style="color:#E45649;">    &quot;@types/react-dom&quot;</span><span style="color:#383A42;">: </span><span style="color:#50A14F;">&quot;^18&quot;</span><span style="color:#383A42;">,</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E45649;">    &quot;eslint&quot;</span><span style="color:#383A42;">: </span><span style="color:#50A14F;">&quot;^8&quot;</span><span style="color:#383A42;">,</span></span>
+<span class="line"><span style="color:#E45649;">    &quot;eslint-config-next&quot;</span><span style="color:#383A42;">: </span><span style="color:#50A14F;">&quot;14.2.4&quot;</span></span>
+<span class="line"><span style="color:#383A42;">  }</span></span>
+<span class="line"><span style="color:#383A42;">}</span></span></code></pre></div>`,3)])])}const d=n(l,[["render",t]]);export{q as __pageData,d as default};
